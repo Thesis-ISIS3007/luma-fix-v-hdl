83f5fa307bdab764951b5f2805ab64246d765622
Isaac David <orpheezt@outlook.com>
2026-04-27 22:45:32 -0500

    Make top-level debug ports optional via core configuration.
    This introduces a debug-gated top-level IO shape so synthesis builds can omit debug outputs when CoreConfig.debug is false.

--- src/Top.scala
+++ src/Top.scala
@@ -2,23 +2,31 @@ package luma_fix_v
 
 import chisel3._
 
+class LumaFixVIO extends Bundle {
+  val imem = new InstrBusIO
+  val dmem = new DataBusIO
+}
+
+class LumaFixVDebugIO extends LumaFixVIO {
+  val debugPC = Output(UInt(32.W))
+  val debugWbValid = Output(Bool())
+  val debugWbRd = Output(UInt(5.W))
+  val debugWbData = Output(UInt(32.W))
+}
+
 class LumaFixV(cfg: CoreConfig = CoreConfig()) extends Module {
-  val io = IO(new Bundle {
-    val imem = new InstrBusIO
-    val dmem = new DataBusIO
-    val debugPC = Output(UInt(32.W))
-    val debugWbValid = Output(Bool())
-    val debugWbRd = Output(UInt(5.W))
-    val debugWbData = Output(UInt(32.W))
-  })
+  val io = if (cfg.debug) IO(new LumaFixVDebugIO) else IO(new LumaFixVIO)
 
   val core = Module(new RV32ICore(cfg))
 
   core.io.imem <> io.imem
   core.io.dmem <> io.dmem
 
-  io.debugPC := core.io.debugPC
-  io.debugWbValid := core.io.debugWbValid
-  io.debugWbRd := core.io.debugWbRd
-  io.debugWbData := core.io.debugWbData
+  if (cfg.debug) {
+    val debugIO = io.asInstanceOf[LumaFixVDebugIO]
+    debugIO.debugPC := core.io.debugPC
+    debugIO.debugWbValid := core.io.debugWbValid
+    debugIO.debugWbRd := core.io.debugWbRd
+    debugIO.debugWbData := core.io.debugWbData
+  }
 }
